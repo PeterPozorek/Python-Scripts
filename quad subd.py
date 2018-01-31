b7fa33bef820ce52f5f42b2e74ebb5d6f7004cd3
PeterPozorek <PeterPozorek@users.noreply.github.com>
2018-01-31 03:50:16 -0700

    Add files via upload

---
+++ quad subd.py
@@ -0,0 +1,49 @@
+﻿import Rhino.Geometry as rg
+
+def subdivide(mesh, recursion, offset1, offset2):
+    mesh2 = rg.Mesh()
+    
+    vtx = mesh.Vertices.ToPoint3dArray()
+    for i in range(len(vtx)):
+        vtx2 = vtx[i] + rg.Vector3d(mesh.Normals[i]) * offset1 # offset vertex
+        mesh2.Vertices.Add(vtx2)
+    
+    for i in range(mesh.Faces.Count):
+        if(mesh.Faces[i].IsQuad): # quad face
+            vidx1 = mesh.Faces[i].A
+            vidx2 = mesh.Faces[i].B
+            vidx3 = mesh.Faces[i].C
+            vidx4 = mesh.Faces[i].D
+            
+            v1 = vtx[vidx1]
+            v2 = vtx[vidx2]
+            v3 = vtx[vidx3]
+            v4 = vtx[vidx4]
+            
+            w1 = (v1+v2)/2 # mid point
+            w2 = (v2+v3)/2 # mid point
+            w3 = (v3+v4)/2 # mid point
+            w4 = (v4+v1)/2 # mid point
+            w5 = (v1+v2+v3+v4)/4 # center
+            w5 += rg.Vector3d(mesh.FaceNormals[i]) * offset2 #offset new vertex
+            
+            widx1 = mesh2.Vertices.Add(w1)
+            widx2 = mesh2.Vertices.Add(w2)
+            widx3 = mesh2.Vertices.Add(w3)
+            widx4 = mesh2.Vertices.Add(w4)
+            widx5 = mesh2.Vertices.Add(w5)
+            mesh2.Faces.AddFace(vidx1,widx1,widx5,widx4)
+            mesh2.Faces.AddFace(vidx2,widx2,widx5,widx1)
+            mesh2.Faces.AddFace(vidx3,widx3,widx5,widx2)
+            mesh2.Faces.AddFace(vidx4,widx4,widx5,widx3)
+            
+        else: # triangular face
+            mesh2.Faces.AddFace(mesh.Faces[i])
+    
+    mesh2.Normals.ComputeNormals()
+    mesh2.Weld(180) #weld vertices
+    if(recursion>1):
+        return subdivide(mesh2, recursion-1, offset1/2, offset2/2)
+    return mesh2
+
+a = subdivide(mesh, recursionLevel, offsetDepth1, offsetDepth2)
---
+++ recursive random branching.py
@@ -0,0 +1,37 @@
+﻿# this creates random branching lines with recursion
+# input type: line - Line (Item Access), div - int (Item Access)
+import rhinoscriptsyntax as rs
+import Rhino.Geometry as rg
+import random
+import math
+
+def recursiveLine(line, depth, axis, resultList):
+    pt1 = line.PointAt(0)
+    pt2 = line.PointAt(1)
+    dir0 = pt2 - pt1
+    dir1 = rg.Vector3d(dir0) # copy
+    dir2 = rg.Vector3d(dir0) # copy
+    dir1.Rotate(math.pi/2, axis)
+    dir1.Rotate(math.pi/2, dir0)
+    dir2.Rotate(-math.pi/2, axis)
+    dir2.Rotate(math.pi/2, dir0)
+    dir1 *= random.random()*0.2 + 0.7; # random scale
+    dir2 *= random.random()*0.2 + 0.7;
+    line1 = rg.Line(pt2, pt2+dir1)
+    line2 = rg.Line(pt2, pt2+dir2)
+    resultList.append(line1)
+    resultList.append(line2)
+    axis = rg.Vector3d.CrossProduct(dir0,dir1)
+    axis.Unitize()
+    box = rg.Box(rg.Plane.WorldXY, [pt1, pt2, pt2+dir1, pt2+dir2, pt2+axis*dir1.Length/2])
+    resultList.append(box)
+    if(depth>0):
+        if(random.random()<0.9): # random omission
+            
+            recursiveLine(line1, depth-1, axis, resultList)
+        if(random.random()<0.8): # random omission
+            #axis = rg.Vector3d.CrossProduct(dir0,dir2)
+            recursiveLine(line2, depth-1, axis, resultList)
+
+a = []
+recursiveLine(line, div, rg.Vector3d.ZAxis, a)
